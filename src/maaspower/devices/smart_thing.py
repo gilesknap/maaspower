cbe0519727ba5521e19486c83e4941dfeaf72af4
Giles Knap <giles.knap@diamond.ac.uk>
2022-02-18 15:00:29 +0000

    Smartthing fully working

--- src/maaspower/devices/smart_thing.py
+++ src/maaspower/devices/smart_thing.py
@@ -9,7 +9,7 @@ from dataclasses import dataclass
 from typing import Literal
 
 import aiohttp
-from pysmartthings import Device, SmartThings
+from pysmartthings import SmartThings
 from typing_extensions import Annotated as A
 
 from maaspower.maasconfig import SwitchDevice
@@ -38,12 +38,44 @@ class SmartThing(SwitchDevice):
 
     def turn_on(self):
         print("turn on", self.on)
+        asyncio.run(self.switch(self.on))
         return MaasResponse.ok
 
     def turn_off(self):
         print("turn off", self.off)
+        asyncio.run(self.switch(self.off))
         return MaasResponse.ok
 
     def query_state(self):
         print("query", self.query)
-        return MaasResponse.on
+        result = asyncio.run(self.switch(self.query, True))
+        if result:
+            return MaasResponse.on
+        else:
+            return MaasResponse.off
+
+    async def switch(self, cmd: str, query: bool = False):
+        async with aiohttp.ClientSession() as session:
+            command = cmd.split(" ")
+
+            api = SmartThings(session, self.api_token)
+            devices = await api.devices()
+            for device in devices:
+                if device.device_id == self.device_id:
+                    if query:
+                        await device.status.refresh()
+                        switch_state = device.status.values.get(command[0])
+                        if switch_state == command[1]:
+                            result = True
+                        elif switch_state == command[2]:
+                            result = False
+                        else:
+                            raise ValueError("unknown device state")
+                    else:
+                        await device.command(*command)
+                        result = True
+                break
+            else:
+                raise ValueError("Device not know to smartThings")
+
+            return result
