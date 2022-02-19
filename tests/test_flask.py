35eae71e5cd0ea9fa514f2f4812acea1aee344fa
Giles Knap <giles.knap@diamond.ac.uk>
2022-02-19 15:31:57 +0000

    add webhook system test

---
+++ tests/test_flask.py
@@ -0,0 +1,53 @@
+"""
+Test the full software stack with the backend switch services mocked
+out.
+"""
+
+from base64 import b64encode
+from pathlib import Path
+
+from ruamel.yaml import YAML
+
+# import all sublasses of SwitchDevice so ApiSchema sees them
+from maaspower.devices.shell_cmd import CommandLine
+from maaspower.devices.smart_thing import SmartThing
+from maaspower.maasconfig import MaasConfig
+from maaspower.webhook import app, load_web_hook
+
+# avoid linter complaints
+required_to_find_subclasses = [SmartThing, CommandLine]
+
+userpass = "a_user:a_pass"
+encoded_u = b64encode(userpass.encode()).decode()
+headers = {"Authorization": "Basic %s" % encoded_u}
+
+
+def test_web_root():
+    """
+    Test that the web server responds to its root URL
+    """
+    with app.test_client() as test_client:
+        response = test_client.get("/")
+        assert response.status_code == 200
+        assert b"MAAS Power Web Hooks Server" == response.data
+
+
+def test_webhook_cmdline(samples: Path):
+    """
+    Load a dummy command line controlled switch configuration and
+    confirm that we can call it via the webhooks
+    """
+
+    dummypath = samples / "dummy_cmd.yaml"
+    config_dict = YAML().load(dummypath)
+
+    maas_config = MaasConfig.deserialize(config_dict)
+    load_web_hook(maas_config)
+
+    with app.test_client() as test_client:
+        response = test_client.post("/maaspower/pi1/on", headers=headers)
+        assert response.status_code == 200
+        response = test_client.post("/maaspower/pi1/off", headers=headers)
+        assert response.status_code == 200
+        response = test_client.post("/maaspower/pi1/query", headers=headers)
+        assert response.data == b"status : running"
