57308fb5e7e5e0d935198ffc2e1b70757e0b6cc6
Giles Knap <giles.knap@diamond.ac.uk>
2022-02-19 15:30:31 +0000

    Add Basic Authentication

--- src/maaspower/webhook.py
+++ src/maaspower/webhook.py
@@ -5,14 +5,14 @@ Use Flask to implement a web server that provides web hooks based on
 the provided configuration
 """
 
-from typing import Dict
-
-from flask import Flask, request
+from flask import Flask
 from flask.wrappers import Response
+from flask_basicauth import BasicAuth
 
 from .maasconfig import MaasConfig
 
 app = Flask("maaspower")
+basic_auth = BasicAuth(app)
 
 
 @app.route("/")
@@ -21,31 +21,37 @@ def hello():
 
 
 @app.route("/maaspower/<devicename>/<command>", methods=["POST", "GET"])
+@basic_auth.required
 def command(devicename: str, command: str):
     """
     Accept webhooks at /maaspower/<devicename>/<command>
     """
     c: MaasConfig = app.config["mass_config"]
     print(f"device: {devicename} command: {command}")
-    if request.authorization is not None:
-        auth: Dict = request.authorization
-        if auth.get("username") != c.username or auth.get("password") != c.password:
-            raise (ValueError("bad credentials"))
-        device = c.find_device(devicename)
-        if device is None:
-            raise ValueError("unknown device")
-        else:
-            result = device.do_command(command)
+    device = c.find_device(devicename)
+    if device is None:
+        raise ValueError("unknown device")
     else:
-        raise (ValueError("no credentials"))
+        result = device.do_command(command)
+
     print(f"response: {result}")
     resp = Response(result)
     return resp
+
+
+def load_web_hook(c: MaasConfig):
+    """
+    Setup config for the webserver
+    """
+    app.config["mass_config"] = c
+    app.config["BASIC_AUTH_USERNAME"] = c.username
+    app.config["BASIC_AUTH_PASSWORD"] = c.password
+    app.config["BASIC_AUTH_FORCE"] = True
 
 
 def run_web_hook(c: MaasConfig):
     """
     Launch the webserver
     """
-    app.config["mass_config"] = c
+    load_web_hook(c)
     app.run(host=c.ip_address, port=c.port)
