bea29124efec02d3914051c9ca330d2f6565c2ad
Giles Knap <giles.knap@diamond.ac.uk>
2022-03-13 09:54:54 +0000

    add new smarthings details

---
+++ tests/smartthingtest.py
@@ -0,0 +1,29 @@
+import asyncio
+
+import aiohttp
+from pysmartthings import SmartThings
+
+print(
+    "If you do not have an API key, generate"
+    " one here: https://account.smartthings.com/tokens"
+)
+
+key = input("\nPlease enter the smartthings API key:\n")
+device_id = input("Please enter the device network ID (blank for ALL):\n")
+print()
+
+
+async def switch():
+    async with aiohttp.ClientSession() as session:
+        api = SmartThings(session, key)
+        devices = await api.devices()
+        if len(devices) == 0:
+            print("no devices for that API key")
+        for device in devices:
+            print("found device: ", device.device_id, device.name, device.label)
+            if device.device_id == device_id or device_id == "":
+                await device.status.refresh()
+                print(" device status is :" + device.status.values.get("switch"))
+
+
+asyncio.run(switch())
